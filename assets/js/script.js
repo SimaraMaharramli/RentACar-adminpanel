ea3e6dc913975eef026d923b65b7feea20d5c0e8
zeyno12 <zeynabbagirova1@gmail.com>
2022-12-14 10:18:35 -0800

    finish

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.getElementById("search").addEventListener("input", filterList);
+function filterList() {
+  const searchInput = document.getElementById("search");
+  const filter = searchInput.value.toLowerCase();
+  const listItems = document.querySelectorAll("tr");
+  listItems.forEach((item) => {
+    let text = item.textContent;
+    if (text.toLowerCase().includes(filter.toLowerCase())) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
+  });
+}
